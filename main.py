3d6ce61424299ea93752baa2aa1e8d902cad6703
mkirslis <mkirs529@gmail.com>
2025-01-13 05:29:22 -0500

    refined existing code for starting program

--- main.py
+++ main.py
@@ -1,82 +1,90 @@
 from config import *
+from databases import *
 from datetime import datetime, timedelta
 import sqlite3
 import os
-import logging
-
-logging.basicConfig(filename="app.log",
-                    level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+import pandas as pd
+import csv
 
 
-def initialize(databases):  # Creates databases & tables or verifies their existence
-    logging.info("Initializing databases...")
+def output(message, log_level="info"):
+    """
+    Print a message to the console and log it simultaneously.
+    
+    Args:
+        message (str): The message to print and log.
+        log_level (str): The logging level as a string (default is "info").
+                         Valid levels: "debug", "info", "warning", "error", "critical".
 
-    for database in databases:
-        try:
-            if os.path.exists(database):
-                logging.info(f"Database '{database}' already exists.")
-            else:
-                try:
-                    with sqlite3.connect(database) as connection:
-                        logging.info(f"Database '{database}' created.")
-                except sqlite3.Error as e:
-                    logging.error(f"Database error: {e}")
-                    continue
-            try:
-                with sqlite3.connect(database) as connection:
-                    cursor = connection.cursor()
-                    connection.execute("BEGIN")
-                    if database in DATABASES:
-                        for table, query in DATABASES[database]["tables"].items():
-                            cursor.execute(
-                                f"SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{table}'")
-                            if cursor.fetchone()[0] == 0:
-                                cursor.execute(query)
-                                logging.info(
-                                    f"Table '{table}' created in database '{database}'.")
-                            else:
-                                logging.info(
-                                    f"Table '{table}' already exists in database '{database}'.")
-                        connection.commit()
-            except sqlite3.Error as e:
-                connection.rollback()
-                logging.error(f"Error creating or verifying tables in database '{
-                              database}': {e}")
-            finally:
-                cursor.close()
-        except OSError as e:
-            logging.error(f"File error: {e}")
+    # Logging Levels:
+    # Level      String       Numeric Value    Description
+    # DEBUG      "debug"      10              Detailed debugging information.
+    # INFO       "info"       20              General operational messages.
+    # WARNING    "warning"    30              Something unexpected or future problems.
+    # ERROR      "error"      40              Serious problem, some functionality failed.
+    # CRITICAL   "critical"   50              Severe error, program may not continue.
+    """
+    log_levels = {
+        "debug": logging.DEBUG,
+        "info": logging.INFO,
+        "warning": logging.WARNING,
+        "error": logging.ERROR,
+        "critical": logging.CRITICAL,
+    }
+    log_level_numeric = log_levels.get(log_level.lower(), logging.INFO)
+    print(message)
+    logging.log(log_level_numeric, message)
 
 
-def update(databases):
-    current_market_day = '2025-01-03'
-    logging.info(f"Latest market data is from {current_market_day}.")
-    for database, items in databases.items():
-        tickers = items.get("tickers", [])
-        tables = items.get("tables", {})
-        logging.info(f"Checking database: {database}...")
-        if not os.path.exists(database):
-            logging.warning(f"Database '{database}' does not exist. Skipping...")
-            continue
-        try:
-            with sqlite3.connect(database) as connection:
-                cursor = connection.cursor()
-                for table in tables.keys():
-                    for ticker in tickers:
-                        try:
-                            query = f"""
-                            SELECT * FROM {table}
-                            WHERE ticker = ? AND t = ?
-                            """
-                            cursor.execute(query, (ticker, current_market_day))
-                            result = cursor.fetchall()
-                            if result:
-                                logging.info(f"{database}, {ticker}, {table} is up to date.")
-                            else:
-                                logging.info(f"{database}, {ticker}, {table} needs updating...")
-                                # Update mechanism here
-                        except sqlite3.Error as e:
-                            logging.error(f"Database error in '{database}': {e}")
-        except sqlite3.Error as e:
-            logging.error(f"Database error in '{database}': {e}")
+def check_database_exists(database: str) -> bool:
+    """Check if the database file exists."""
+    try:
+        if os.path.exists(database):
+            output(f"Database '{database}' already exists.")
+            return True
+        else:
+            output(f"Database '{database}' does not exist.")
+            return False
+    except OSError as e:
+        output(f"Error checking for database '{database}': {e}", "error")
+        return True
+
+
+def create_database(database: str):
+    """Create the database (used if the dt does not exist)."""
+    try:
+        with sqlite3.connect(database):
+            output(f"Database '{database}' created.")
+    except sqlite3.Error as e:
+        output(f"Error creating database '{database}': {e}", "error")
+
+
+def check_table_exists(connection: sqlite3.Connection, database: str, table_name: str) -> bool:
+    """Check if a table exists in the database."""
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'")
+        result = cursor.fetchone()
+        if result:
+            output(f"Table '{database}, {table_name}' already exists.")
+            return True
+        else:
+            output(f"Table '{database}, {table_name}' does not exist.")
+            return False
+    except sqlite3.Error as e:
+        output(f"Error checking table '{database}, {table_name}': {e}", "error")
+        return True
+    finally:
+        cursor.close()
+
+
+def create_table(connection: sqlite3.Connection, database: str, table_name: str, query: str):
+    """Create a table in the database if it does not exist."""
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        output(f"Table '{database}, {table_name}' created.")
+    except sqlite3.Error as e:
+        output(f"Error creating table '{database}, {table_name}': {e}", "error")
+    finally:
+        cursor.close()
--- run.py
+++ run.py
@@ -1,25 +1,33 @@
 from datetime import datetime
 from config import *
+from databases import *
 from main import *
+
 
 def main():
     start = datetime.now()
     formatted_start = start.strftime("%Y-%m-%d %H:%M:%S")
-
-    print(f"\n{PROJECT_NAME} v{VERSION_NUMBER} - {formatted_start}")
-    logging.info(f"{PROJECT_NAME} v{VERSION_NUMBER} - {formatted_start}")
     
-    initialize(DATABASES)  # Creates databases & tables or verifies their existence
-    update(DATABASES)  # Updates database tables or verifies they are up to date
+    output(f"{PROJECT_NAME} v{VERSION_NUMBER}")
+    output(f"Started: {formatted_start}")
+    output("Initailizing databases...")
 
-    end_time = datetime.now() 
+    for database, items in DATABASES.items():
+        if not check_database_exists(database):
+            create_database(database)
+
+        with sqlite3.connect(database) as connection:
+            for table, query in items["tables"].items():
+                if not check_table_exists(connection, database, table):
+                    create_table(connection, database, table, query)
+
+    end_time = datetime.now()
     runtime = end_time - start
     formatted_end_time = end_time.strftime("%Y-%m-%d %H:%M:%S")
 
-    print(f"\nProgram completed - {formatted_end_time}")
-    print(f"\nRuntime: {runtime}\n")
-    logging.info(f"Program completed - {formatted_end_time}")
-    logging.info(f"Runtime: {runtime}")
+    output(f"Exited: {formatted_end_time}")
+    output(f"Runtime: {runtime}")
+
 
 if __name__ == "__main__":
     main()
