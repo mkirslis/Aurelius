b27126432acc649525e1062aed5532a1d8815c56
Matthew Kirslis <mkirs529@gmail.com>
2025-01-18 22:55:29 -0500

    simplification

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
-from config import *
-from databases import *
+from config import API_KEY
+import logging
+import sqlite3
+import requests
 from datetime import datetime, timezone
 import time
-import sqlite3
 import os
-import pandas as pd
-import pytz
-import logging
-import requests
 
 PROJECT_NAME = "Aurelius"
-VERSION_NUMBER = "0.0.4"
-EARLIEST_MARKET_DATE = "2023-01-17"
-LATEST_MARKET_DATE = "2025-01-15"
-
-logging.basicConfig(filename=f"log_v{VERSION_NUMBER}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log",
-                    level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+VERSION_NUMBER = "0.0.55"
+START_DATE = "2023-01-18"
+END_DATE = "2025-01-17"
+DATABASES = ['stocks.db', 'backup_stocks.db']
+TABLES = ['ohlcv_daily']
+TICKERS = ['JPM', 'MS',  'GS', 'BAC', 'C', 'WFC']
 
 
 def output(message, log_level="info"):
     """
-    Print & log simultaneously.
-
+    Prints & logs a message simultaneously.
     # Logging Levels:
     # Level      String       Value           Description
     # DEBUG      "debug"      10              Detailed debugging information.
@@ -43,100 +38,19 @@ def output(message, log_level="info"):
     logging.log(log_level_numeric, message)
 
 
-def check_database_exists(database: str) -> bool:
-    """Check if a database file exists."""
-    try:
-        if os.path.exists(database):
-            output(f"Database '{database}' already exists.")
-            return True
-        else:
-            output(f"Database '{database}' does not exist.")
-            return False
-    except OSError as e:
-        output(f"Error checking for database '{database}': {e}", "error")
-        return True
-
-
-def create_database(database: str):
-    """Create a database file."""
-    try:
-        with sqlite3.connect(database):
-            output(f"Database '{database}' created.")
-    except sqlite3.Error as e:
-        output(f"Error creating database '{database}': {e}", "error")
-
-
-def check_table_exists(connection: sqlite3.Connection, database: str, table_name: str) -> bool:
-    """Check if a table exists in a database."""
-    cursor = connection.cursor()
-    try:
-        cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'")
-        result = cursor.fetchone()
-        if result:
-            output(f"Table '{database}, {table_name}' already exists.")
-            return True
-        else:
-            output(f"Table '{database}, {table_name}' does not exist.")
-            return False
-    except sqlite3.Error as e:
-        output(f"Error checking table '{database}, {table_name}': {e}", "error")
-        return True
-    finally:
-        cursor.close()
-
-
-def create_table(connection: sqlite3.Connection, database: str, table_name: str, query: str):
-    """Create a table in a database."""
-    try:
-        cursor = connection.cursor()
-        cursor.execute(query)
-        output(f"Table '{database}, {table_name}' created.")
-    except sqlite3.Error as e:
-        output(f"Error creating table '{database}, {table_name}': {e}", "error")
-    finally:
-        cursor.close()
-
-
-def aggregate_bars_baseline(url):
-    """Pulls timestamps for $SPY aggregates to compare against other tickers."""
-    time.sleep(13)  # API limits 5 requests per minute
-    
-    # If any old timestamps exist before the most recent current date
-    # Keep them by removing all timestamps greater than latest date before pull
-
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        connection = sqlite3.connect("BaselineTimestamps.db")
-        cursor = connection.cursor()
-        for result in data['results']:
-            cursor.execute('''
-                INSERT INTO baseline_timestamps (timestamp)
-                VALUES (?)
-            ''', (
-                result['t'],
-            ))
-        connection.commit()
-        connection.close()
-        output("Pulled baseline timestamps from $SPY.")
-        return data
-    else:
-        return f"Error: {response.status_code}"
-
-
-def aggregate_bars(url, database, table):
+def aggregate_daily_bars(connection, url, database, table, ticker):
     """Pulls daily OHLCV data from Polygon IO."""
-    time.sleep(13) # API limits 5 requests per minute
+    time.sleep(13)  # API limits 5 requests per minute
+    connection = sqlite3.connect(f"{database}")
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        connection = sqlite3.connect(f"{database}")
         cursor = connection.cursor()
         for result in data['results']:
             dt = datetime.fromtimestamp(result['t'] / 1000, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
             cursor.execute(f'''
-                INSERT INTO {table} (timestamp, datetime, ticker, open, high, low, close, volume, volume_weighted, trades)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                INSERT OR IGNORE INTO {table} (timestamp, datetime, ticker, open, high, low, close, volume, volume_weighted, trades, resultsCount, request_id)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
             ''', (
                 result['t'],
                 dt,
@@ -147,10 +61,74 @@ def aggregate_bars(url, database, table):
                 result['c'],
                 result['v'],
                 result['vw'],
-                result['n']
+                result['n'],
+                data['resultsCount'],
+                data['request_id']
             ))
         connection.commit()
         connection.close()
         return data
     else:
         return f"Error: {response.status_code}"
+
+
+def main():
+    """Logging"""
+    os.makedirs('logs', exist_ok=True)
+    logging.basicConfig(filename=f"logs/log_v{VERSION_NUMBER}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log",
+                        level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+
+    """Start"""
+    start_time = datetime.now()
+    start_datetime = start_time.strftime("%Y-%m-%d %H:%M:%S")
+    output(f"{PROJECT_NAME} v{VERSION_NUMBER}")
+    output(f"Program started: {start_datetime}")
+
+    """Initialization"""
+    for database in DATABASES:
+        for table in TABLES:
+            try:
+                connection = sqlite3.connect(f"{database}")
+                cursor = connection.cursor()
+                output(f"Initializing {database} & {table}...")
+                cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS ohlcv_daily (
+                        timestamp INTEGER,
+                        datetime DATETIME,
+                        ticker TEXT,
+                        open REAL,
+                        high REAL,
+                        low REAL,
+                        close REAL,
+                        volume INTEGER,
+                        volume_weighted REAL,
+                        trades INTEGER,
+                        resultsCount INTEGER,
+                        request_id TEXT )
+                    ''')
+                output(f"{database} & {table} have been initialized.")
+            except sqlite3.Error as e:
+                output(f"Error initializing {database} & {table}: {e}")
+            finally:
+                cursor.close()
+
+    """Procurement"""
+    for database in DATABASES:
+        for table in TABLES:
+            for ticker in TICKERS:
+                url = f"https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/day/{START_DATE}/{END_DATE}?apiKey={API_KEY}"
+                output(f"Procuring data for {database}, {table}, {ticker}...")
+                aggregate_daily_bars(connection, url, database, table, ticker)
+                output(f"Data procured for {database}, {table}, {ticker}.")
+
+    """End"""
+    end_time = datetime.now()
+    runtime = end_time - start_time
+    end_datetime = end_time.strftime("%Y-%m-%d %H:%M:%S")
+    output(f"Program ended: {end_datetime}")
+    output(f"Runtime: {runtime}")
+
+
+if __name__ == "__main__":
+    main()
